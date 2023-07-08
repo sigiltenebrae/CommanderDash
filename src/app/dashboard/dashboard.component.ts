fd031c8b679e3895f83437aaf4220e644150dd93
sigiltenebrae <ck6202@rit.edu>
2023-07-08 13:53:15 -0400

    switched over to apexcharts

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,5 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import { ChartComponent } from "ng-apexcharts";
-import {
-  ApexNonAxisChartSeries,
-  ApexResponsive,
-  ApexChart,
-  ApexFill,
-  ApexDataLabels,
-  ApexLegend
-} from "ng-apexcharts";
 
 @Component({
   selector: 'app-dashboard',
@@ -52,41 +44,161 @@ export class DashboardComponent implements OnInit{
   mtgColorBackground = {white: '#eeeeeebb', blue: '#64b5f6bb', black: '#9e9e9ebb', red: '#e57373bb', green: '#81c784bb'}
   mtgColorBorder = {white: '#e0e0e0bb', blue: '#42a5f5bb', black: '#757575bb', red: '#ef5350bb', green: '#66bb6abb'}
 
-  @ViewChild("chart") chart: ChartComponent;
-  public chartOptions: any;
+  themeTextColor = 'rgba(255, 255, 255, 0.8)'
+
+  //@ViewChild("chart") colorDistChart: ChartComponent;
+  public colorDistChartOptions: any;
+  public winLossChartOptions: any;
 
   ngOnInit(): void {
   }
 
   constructor() {
-    this.chartOptions = {
-      series: [44, 55, 41, 17, 15],
+    this.colorDistChartOptions = {
+      series: [
+        {
+          name: "Sigiltenebrae",
+          data: [80, 50, 30, 40, 100]
+        },
+        {
+          name: "All Players",
+          data: [20, 30, 40, 80, 20]
+        },
+      ],
       chart: {
-        width: 380,
-        type: "donut"
+        toolbar: {
+          show: false
+        },
+        height: 350,
+        type: "radar",
+        dropShadow: {
+          enabled: true,
+          blur: 1,
+          left: 1,
+          top: 1
+        },
+        foreColor: this.themeTextColor
       },
-      dataLabels: {
-        enabled: false
+      title: {
+        text: "Radar Chart - Multi Series",
+      },
+      stroke: {
+        show: false,
       },
       fill: {
-        type: "gradient"
+        opacity: 0.4
       },
-      legend: {
-        show: false
+      markers: {
+        size: 0
       },
-      responsive: [
+      xaxis: {
+        categories: ["White", "Blue", "Black", "Red", "Green"],
+        labels: {
+          style: {
+            fontSize: 12,
+            colors: [this.mtgColorBackground.white, this.mtgColorBackground.blue, this.mtgColorBackground.black, this.mtgColorBackground.red, this.mtgColorBackground.green]
+          }
+        }
+      },
+      yaxis: {
+        show: false,
+        labels: {
+          show: false,
+        }
+      },
+    };
+    this.winLossChartOptions = {
+      series: [
         {
-          breakpoint: 480,
-          options: {
-            chart: {
-              width: 200
-            },
-            legend: {
-              position: "bottom"
+          name: "Inflation",
+          data: [2.3, 0.6, 1.0]
+        }
+      ],
+      chart: {
+        toolbar: {
+          show: false
+        },
+        height: 300,
+        type: "bar",
+        foreColor: this.themeTextColor
+      },
+      plotOptions: {
+        bar: {
+          dataLabels: {
+            position: "top" // top, center, bottom
+          }
+        }
+      },
+      dataLabels: {
+        enabled: true,
+        offsetY: -20,
+        style: {
+          fontSize: "12px",
+        }
+      },
+
+      xaxis: {
+        categories: [
+          "This Deck",
+          "Sigiltenebrae",
+          "Average",
+        ],
+        position: "bottom",
+        axisBorder: {
+          show: false
+        },
+        axisTicks: {
+          show: false
+        },
+        crosshairs: {
+          fill: {
+            type: "gradient",
+            gradient: {
+              colorFrom: "#D8E3F0",
+              colorTo: "#BED1E6",
+              stops: [0, 100],
+              opacityFrom: 0.4,
+              opacityTo: 0.5
             }
           }
         }
-      ]
-    };
+      },
+      fill: {
+        type: "gradient",
+        gradient: {
+          shade: "light",
+          type: "horizontal",
+          shadeIntensity: 0.25,
+          gradientToColors: undefined,
+          inverseColors: true,
+          opacityFrom: 1,
+          opacityTo: 1,
+          stops: [50, 0, 100, 100]
+        }
+      },
+      yaxis: {
+        axisBorder: {
+          show: false
+        },
+        axisTicks: {
+          show: false
+        },
+        labels: {
+          show: false,
+          formatter: function(val: any) {
+            return val + "%";
+          }
+        }
+      },
+      title: {
+        show: true,
+        text: "Win/Loss Ratio",
+        floating: 0,
+        align: "center",
+      },
+      tooltip: {
+        enabled: false
+      }
+    }
   }
 }
